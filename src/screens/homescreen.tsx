532ea67bd3ec6ced953bdc95855e7fb320449498
dgonasc <50498536+dgonasc@users.noreply.github.com>
2023-07-17 20:07:41 -0300

    commit adjustments homescreen

--- src/screens/homescreen.tsx
+++ src/screens/homescreen.tsx
@@ -26,10 +26,10 @@ export default function HomeScreen() {
         <>
         <div className="grid items-start justify-center font-noto">
         {/* mx-6 my-10 lg:mx-36 2xl:mx-52 */}
-            <div className="mx-8 my-8 lg:mb-16 lg:mx-36">
-                <h3 className="m-5 text-3xl xl:text-6xl font-xilosa drop-shadow-xl">Terno do Binga</h3>
-                <p className="mb-10 text-lg xl:text-xl xl:text-left">O Terno do Binga é fruto do encontro do efervescente movimento das fanfarras belorizontinas e os grupos percussivos que pesquisam as culturas populares tradicionais brasileiras na cidade. O Terno do Binga é uma orquestra diferente, cuja maior inspiração são os ternos de Maracatu de Baque Solto e as orquestras cirandeiras, do Frevo e o Coco Rural.</p>
-                    <Link href="/about" className="p-4 text-black border-2 border-solid rounded-md shadow-lg hover:opacity-60 text-md font-secondary hover:bg-corterceira/30">
+            <div className="mx-8 my-8 max-sm:my-1 max-sm:mb-8 lg:mb-16 lg:mx-36">
+                <h3 className="m-5 text-3xl max-sm:m-8 max-sm:text-xl xl:text-6xl font-xilosa drop-shadow-xl">Terno do Binga</h3>
+                <p className="mb-10 text-lg max-sm:m-8 max-sm:text-xs xl:text-xl xl:text-left">O Terno do Binga é fruto do encontro do efervescente movimento das fanfarras belorizontinas e os grupos percussivos que pesquisam as culturas populares tradicionais brasileiras na cidade. O Terno do Binga é uma orquestra diferente, cuja maior inspiração são os ternos de Maracatu de Baque Solto e as orquestras cirandeiras, do Frevo e o Coco Rural.</p>
+                    <Link href="/about" className="p-4 text-black border-2 border-solid rounded-md shadow-lg max-sm:ml-8 max-sm:p-2 hover:opacity-60 text-md font-secondary hover:bg-corterceira/30">
                     <button>
                         Saiba mais
                     </button>
@@ -43,7 +43,7 @@ export default function HomeScreen() {
                 muted
                 src="video_home.mov"
                 ref={videoEl}
-                className="flex object-cover w-screen m-0 shadow-2xl lg:h-96 h-60 opacity-80"
+                className="flex object-cover w-full shadow-2xl lg:h-96 h-60 opacity-80"
                 />
             </div>
             <div className="flex justify-center mx-16 my-16 text-center lg:mx-36">
