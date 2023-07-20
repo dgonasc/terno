738ae03a1ce55004534f75b86c0a806ef52e1026
dgonasc <50498536+dgonasc@users.noreply.github.com>
2023-07-20 11:19:18 -0300

    adjustments

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -46,10 +46,10 @@ export default function ContactForm() {
 
   return (
       <form onSubmit={handleSubmit} className='grid justify-center text-gray-700 font-xilosa'>
-        <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder='Nome' className='p-2 mt-3 border-2 border-yellow-400 rounded-lg w-72' />
-        <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder='email' className='p-2 mt-3 border-2 border-yellow-400 rounded-lg w-72' />
-        <textarea name="message" value={formData.message} onChange={handleChange} placeholder='mensagem' className='p-2 mt-3 border-2 border-yellow-400 rounded-lg h-36 w-72' />
-        <button type="submit" className='mx-20 mt-4 text-white uppercase bg-gray-500 border-2 border-yellow-600 rounded-lg shadow-md shadow-gray-500'>Enviar</button>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder='Nome' className='p-2 mt-3 border-2 border-yellow-200 rounded-lg w-72' />
+        <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder='email' className='p-2 mt-3 border-2 border-yellow-200 rounded-lg w-72' />
+        <textarea name="message" value={formData.message} onChange={handleChange} placeholder='mensagem' className='p-2 mt-3 border-2 border-yellow-200 rounded-lg h-36 w-72' />
+        <button type="submit" className='mx-20 mt-4 text-white uppercase bg-gray-500 border-2 border-yellow-300 rounded-lg shadow-md shadow-gray-500'>Enviar</button>
       </form>
   )
 }
