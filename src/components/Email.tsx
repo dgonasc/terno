e1d0494851c79519c82b92d35ae8710be2d94c57
dgonasc <50498536+dgonasc@users.noreply.github.com>
2023-08-10 16:58:50 -0300

    commit homescreen correction

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -90,7 +90,7 @@ export default function ContactForm() {
           alt="Binga e o Sol"
           width={300}
           height={200}
-          className="w-full mb-20 ml-12 opacity-70 drop-shadow-2xl"
+          className="w-full mb-20 ml-12 max-lg:ml-2 max-lg:justify-center opacity-70 drop-shadow-2xl"
         />
       </div>
     </div>
