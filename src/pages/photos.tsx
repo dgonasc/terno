2fc84aa475ab169a9fc2b53a593397c11bf03a35
dgonasc <50498536+dgonasc@users.noreply.github.com>
2023-09-08 10:42:27 -0300

    adjustments for meta

--- src/pages/photos.tsx
+++ src/pages/photos.tsx
@@ -58,13 +58,13 @@ export default function ModalPhotos(props: { photos: any }) {
         <div>
             <Head>
                 <title>Fotos - Terno do Binga</title>
-                <meta name="description" content="O Terno do Binga é uma orquestra diferente, inspirada nos ternos de Maracatu de Baque Solto e nas orquestras de Frevo." key="desc" />
+                <meta name="description" content="Uma orquestra diferente, inspirada nos ternos de Maracatu de Baque Solto e nas orquestras de Frevo." key="desc" />
                 <meta property="og:title" content="Terno do Binga" />
                 <meta property="og:image" content="https://terno-do-binga.s3.us-east-2.amazonaws.com/files/BINGA_AVATAR_RAINBOW.PNG" />
                 <meta property="og:url" content="https://www.ternodobinga.com.br" />
                 <meta property="twitter:image" content="https://terno-do-binga.s3.us-east-2.amazonaws.com/files/BINGA_AVATAR_RAINBOW.PNG"/>
                 <meta property="twitter:card" content="https://terno-do-binga.s3.us-east-2.amazonaws.com/files/BINGA_AVATAR_RAINBOW.PNG"/>
-                <meta property="twitter:description" content="O Terno do Binga é uma orquestra diferente, inspirada nos ternos de Maracatu de Baque Solto e nas orquestras de Frevo." key="desc"/>
+                <meta property="twitter:description" content="Uma orquestra diferente, inspirada nos ternos de Maracatu de Baque Solto e nas orquestras de Frevo." key="desc"/>
                 <meta property="twitter:title" content="Terno do Binga"/>
             </Head>
             <Menu />
