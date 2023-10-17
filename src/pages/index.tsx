4d5657cac312809a6fde6e22a54a02dafa92b337
dgonasc <50498536+dgonasc@users.noreply.github.com>
2023-10-16 22:33:54 -0300

    commit tag analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import Menu from "@/components/Menu"
-import HomeScreen from "@/screens/homescreen"
-import Head from "next/head"
+import Menu from "@/components/Menu";
+import HomeScreen from "@/screens/homescreen";
+import Head from "next/head";
+import Script from 'next/script'
 
 export default function Home () {
   return (
@@ -8,6 +9,18 @@ export default function Home () {
       <Head>
         <title>Terno do Binga</title>
       </Head>
+      <div className="container">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-0V5X4EV531" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0V5X4EV531');
+          `}
+        </Script>
+      </div>
       <Menu />
       <HomeScreen />
     </>
