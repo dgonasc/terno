dd47c3ffd1d566de1faf80d58bb3579f2f350f52
dgonasc <50498536+dgonasc@users.noreply.github.com>
2024-05-10 20:19:59 -0300

    bug correction

--- .next/static/webpack/webpack.444d3d0332d72ee3.hot-update.js
+++ .next/static/webpack/webpack.444d3d0332d72ee3.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "75cf1e23e2ea0026"; }
+/******/ 	__webpack_require__.h = function() { return "4e89d543271141ff"; }
 /******/ }();
 /******/ 
 /******/ }
